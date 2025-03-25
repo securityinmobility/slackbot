92ef43716ee5d60b139b384ca2f93deaf491660d
Dominik Bayerl <dominik.bayerl@carissma.eu>
2025-03-25 11:44:33 +0100

    Hotfix: urllib requires bytes() for POST-data
    Signed-off-by: Dominik Bayerl <dominik.bayerl@carissma.eu>

--- sinks.py
+++ sinks.py
@@ -1,17 +1,27 @@
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
 from urllib import request as req
 import json
 
 
 def slack_sink(message, channel, token):
+    from slack_sdk import WebClient
+    from slack_sdk.errors import SlackApiError
+    
     client = WebClient(token=token)
     response = client.chat_postMessage(channel=channel, text=message)
     return response
 
 def teams_sink(message, url):
+    payload = {
+        "type": "message",
+        "attachments": [
+            {
+                "contentType": "application/vnd.microsoft.card.adaptive",
+                "content": json.loads(message),
+            }
+        ]
+    }
     request = req.Request(url=url, method="POST")
     request.add_header(key="Content-Type", val="application/json")
-    with req.urlopen(url=request, data=message) as response:
+    with req.urlopen(url=request, data=json.dumps(message).encode()) as response:
         if response.status != 200:
             raise Exception(response.reason)
